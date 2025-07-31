2839bb7101d7426011d8a58393d9a3da390f9405
Patel Prince <princepatel6626@gmail.com>
2025-07-31 22:03:21 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -34,12 +34,6 @@ const About = () => {
             </p>
           </div>
         </div>
-
-        {/* Team Section */}
-        <div className="text-center mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-8">Meet Our Team</h2>
-          <User name={"PS"} location={"Mehsana"} />
-        </div>
       </div>
     </div>
   );
